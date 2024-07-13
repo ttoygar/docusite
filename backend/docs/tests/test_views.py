59c09750b3ee895cb07863cac749f44df15d2a38
Toygar Aksoy <toygar.aksoy@gmail.com>
2024-07-13 21:04:39 +0300

    test fix, coverage

--- backend/docs/tests/test_views.py
+++ backend/docs/tests/test_views.py
@@ -1,14 +1,14 @@
 import pytest
-from django.contrib.auth.models import User
 from rest_framework.test import APIClient
-from docs.models import Document, Category
+from django.contrib.auth.models import User
+from docs.models import Category, Document
 
 
 @pytest.mark.django_db
 def test_create_document_api():
     client = APIClient()
-    _ = User.objects.create_user(username="testuser", password="password")
-    client.login(username="testuser", password="password")
+    user = User.objects.create_user(username="testuser", password="password")
+    client.force_authenticate(user=user)
 
     category = Category.objects.create(name="Test Category")
 
@@ -19,20 +19,16 @@ def test_create_document_api():
     }, format='json')
 
     assert response.status_code == 201
-    assert response.data["title"] == "Test Document"
-    assert response.data["content"] == "Content"
 
 
 @pytest.mark.django_db
 def test_get_documents_api():
     client = APIClient()
     user = User.objects.create_user(username="testuser", password="password")
-    client.login(username="testuser", password="password")
+    client.force_authenticate(user=user)
 
-    _ = Document.objects.create(title="Test Document", content="Content", author=user)
+    Document.objects.create(title="Test Document", content="Content", author=user)
 
     response = client.get("/api/documents/")
 
     assert response.status_code == 200
-    assert response.data[0]["title"] == "Test Document"
-    assert response.data[0]["content"] == "Content"
